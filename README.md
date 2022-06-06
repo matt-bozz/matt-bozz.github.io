439fc4fda8c94b6c508220bd08bb015fd58bb402
mattb <mattb@ql-research.com>
2022-06-05 21:35:45 -0400

    attempting threejs

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # matt-bozz.github.io
 Resume website
+
+## Dev
+```bash
+npm run dev
+```
+
+## Deploy
+```bash
+npm run deploy
+git add .
+git commit 
+git push
+```
+
+https://matt-bozz.github.io/
--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 import './style.css'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry(10,3,16,100);
+const material = new THREE.MeshBasicMaterial( { color : 0xFF6347, wireframe: true} );
+const torus = new THREE.Mesh(geometry, material);
+scene.add(torus);
+
+function resizeCanvasToDisplaySize() {
+  const canvas = renderer.domElement;
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  // adjust displayBuffer size to match
+  if (canvas.width !== width || canvas.height !== height) {
+    // you must pass false here or three.js sadly fights the browser
+    renderer.setSize(width, height, false);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+
+    // update any render target sizes here
+  }
+}
+function animate(){
+  resizeCanvasToDisplaySize();
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+animate();
